a1638e8062d2d3fbe59a3f4ccd3b152e95d0e2a5
Junichiro <c.shimomoto@gmail.com>
2026-03-19 17:24:20 +0900

    Step08_Input Add

---
+++ Step08_Input.py
@@ -0,0 +1,14 @@
+input_msg = input('Insert Message:')
+
+print(f'Accepted Message: {input_msg}')
+
+input_name: str = input("Enter Name:")
+input_addr: str = input('Registered Address:')
+
+print(f'Name:{input_name} Address: {input_addr}')
+
+input_age: str = input('My Age:')
+
+age:int = int(input_age) + 1
+
+print('Your Birth Year is {age} years old')
